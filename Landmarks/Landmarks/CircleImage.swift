c6eb64d2e238aaba1400badcac56dbe31a39f190
최하은 <haeun4345@gmail.com>
2023-09-23 11:47:40 +0900

    Creating and Combining Views

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 최하은 on 2023/09/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
