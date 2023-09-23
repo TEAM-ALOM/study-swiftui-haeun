8cd980c5453fd70f23908c011a78e0c2d333cde2
최하은 <haeun4345@gmail.com>
2023-09-23 15:09:19 +0900

    Building Lists and Navigation

---
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 최하은 on 2023/09/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
