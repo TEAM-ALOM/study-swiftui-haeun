0e7c0001427ff55576eee3e9f4560b3e06006a82
최하은 <haeun4345@gmail.com>
2023-09-24 12:50:00 +0900

    Handling User Input

---
+++ Landmarks/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by 최하은 on 2023/09/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+        
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
--- Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+          modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+      }
     
     var body: some View {
         ScrollView {
@@ -23,9 +28,11 @@ struct LandmarkDetail: View {
 
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
 
                 HStack {
                     Text(landmark.park)
@@ -50,6 +57,12 @@ struct LandmarkDetail: View {
     }
 }
 
-#Preview {
-    LandmarkDetail(landmark: landmarks[0])
+struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+
+
+    static var previews: some View {
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+    }
 }
