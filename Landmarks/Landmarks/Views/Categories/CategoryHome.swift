a7bb504578fe446d128811aae0b48e4118ce5d8b
최하은 <haeun4345@gmail.com>
2023-09-28 22:34:07 +0900

    Composing Complex Interfaces

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 최하은 on 2023/09/28.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environmentObject(ModelData())
+}
