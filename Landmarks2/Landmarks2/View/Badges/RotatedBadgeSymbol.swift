2e9a57f18d9f6affc51779af33f8376092341853
최하은 <haeun4345@gmail.com>
2023-10-08 18:24:52 +0900

    Drawing Path

---
+++ Landmarks2/Landmarks2/View/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks2
+//
+//  Created by 최하은 on 2023/10/08.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
